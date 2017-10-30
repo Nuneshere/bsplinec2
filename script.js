dc21394ff5a1ccc9477a4927ade46d8d0ae164ea
Mateusnunes <mateusnunes@me.com>
2017-10-30 17:02:52 -0300

    COMITAO DO SUCESSO

--- script.js
+++ script.js
@@ -7,76 +7,97 @@ var points = [];
 var move = false;
 var index = -1;
 var qntPontos = 0;
-var precisao = 100, parametro = 1 / precisao;
+var grau = qntPontos - 1;
+var precisao = 1000, parametro = 1 / precisao;
 
 // CASTEJAU
 
-function makingCurve() {
-        var grau = qntPontos-1;
-        var pontoDaCurva[];
-    
-        while (parametro < 1) {
-            var pontosCasteljau=[];
-            pontosCasteljau = points.slice(0,qntPontos + 1);
 
-            for (var n = 0 ; qntPontos-1 !== 0 ; n++){
 
-            }
-        parametro = parametro + 1 / precisao;
+function makeCurva(){
+    var pointsCurve = [];
+    for (t = 0 ; t <= 1 ; t = t + parametro){
+        var pontosCastel = points.slice(0, qntPontos+1);
+        for(n = 1; n < qntPontos ; n++) {
+          for(p = 0; p < qntPontos - n; p++) {
+            var cordX = (1 - t) * pontosCastel[p].x + t * pontosCastel[p+1].x;
+            var cordY = (1 - t) * pontosCastel[p].y + t * pontosCastel[p+1].y;
+            pontosCastel[p] = {x: cordX, y: cordY};
+          }
+        }
+        pointsCurve.push(pontosCastel[0]);
+    }
+    drawCurve(pointsCurve);
+}
+
+function drawCurve(pointsCurve) {
+  if(qntPontos > 3) {
+    for(var i in pointsCurve) {
+      ctx.beginPath();
+      
+      if(i > 0) {
+        var xAtual = pointsCurve[i-1].x;
+        var yAtual = pointsCurve[i-1].y;
+        ctx.moveTo(xAtual, yAtual);
+        ctx.lineTo(pointsCurve[i].x, pointsCurve[i].y);
+         ctx.strokeStyle= '#7B1FA2';
+        ctx.lineWidth=3;
+        ctx.stroke();
       }
-};
-
+    }
+  }
+}
 
 // FUNCOES BASICAS DE CANVAS-----------------
 function clearCanvas()
 {
-    var canvas = document.getElementById('canvas'),
-        ctx = canvas.getContext("2d");
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    points.splice(0,points.length);
-    drawCircles();
+  var canvas = document.getElementById('canvas'),
+      ctx = canvas.getContext("2d");
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  points.splice(0,points.length);
+  p
+  drawCircles();
 }
 
 function resizeCanvas() {
-	canvas.width = parseFloat(window.getComputedStyle(canvas).width);
-	canvas.height = parseFloat(window.getComputedStyle(canvas).height);
+  canvas.width = parseFloat(window.getComputedStyle(canvas).width);
+  canvas.height = parseFloat(window.getComputedStyle(canvas).height);
 }
 
 function drawCircles() {
-    qntPontos = qntPontos + 1 ; 
-	for (var i in points) {
-		ctx.beginPath();
-		ctx.arc(points[i].x, points[i].y, 6, 0, 2 * Math.PI);
-		ctx.fillStyle = '#64B5F6';
-		ctx.fill();
-	
-	
-		if(i>0){
-			var posX= points[i-1].x;
-			var posY= points[i-1].y;
+  for (var i in points) {
+    ctx.beginPath();
+    ctx.arc(points[i].x, points[i].y, 6, 0, 2 * Math.PI);
+    ctx.fillStyle = '#F8BBD0';
+    ctx.fill();
 
-			ctx.moveTo(posX,posY);
-			ctx.lineTo(points[i].x,points[i].y)
-            ;
-            ctx.strokeStyle= '#64B5F6';
-            ctx.lineWidth=3;
-			ctx.stroke();
-		}
-	}
+    if(i>0){
+        var posX= points[i-1].x;
+        var posY= points[i-1].y;
+        ctx.moveTo(posX,posY);
+        ctx.lineTo(points[i].x,points[i].y);
+        ctx.strokeStyle= '#F8BBD0';
+        ctx.lineWidth=3;
+        ctx.stroke();
+    }
+  }
+  if(qntPontos > 3) {
+    makeCurva();
+  }
 }
 
 function dist(p1, p2) {
-	var v = {x: p1.x - p2.x, y: p1.y - p2.y};
-	return Math.sqrt(v.x * v.x + v.y * v.y);
+  var v = {x: p1.x - p2.x, y: p1.y - p2.y};
+  return Math.sqrt(v.x * v.x + v.y * v.y);
 }
 
 function getIndex(click) {
-	for (var i in points) {
-		if (dist(points[i], click) <= 10) {
-			return i;
-		}
-	}
-	return -1;
+  for (var i in points) {
+    if (dist(points[i], click) <= 10) {
+      return i;
+    }
+  }
+  return -1;
 }
 
 
@@ -90,43 +111,44 @@ resizeCanvas();
 
 
 canvas.addEventListener('mousedown', e => {
-	var click = {x: e.offsetX, y: e.offsetY, v:{x: 0, y:0}};
-	index = getIndex(click);
-	if (index === -1) {
-		points.push(click);
-		drawCircles();
-	} else {
-		move = true;
-	}
+  var click = {x: e.offsetX, y: e.offsetY, v:{x: 0, y:0}};
+  index = getIndex(click);
+  if (index === -1) {
+    qntPontos++;
+    points.push(click);
+    drawCircles();
+  } else {
+    move = true;
+  }
 });
 
 
 canvas.addEventListener('mouseup', e => {
-	move = false;
+  move = false;
 });
 
 canvas.addEventListener('dblclick', e => {
-	if (index !== -1) {
-		points.splice(index, 1);
-	}
+  if (index !== -1) {
+    points.splice(index, 1);
+    qntPontos--;
+  }
 });
 
 
 canvas.addEventListener('mousemove', e => {
-	if (move) {
-		var old = points[index];
-		points[index] = {x: e.offsetX, y: e.offsetY, v: {x: 0, y: 0}};
-		points[index].v = {x: e.offsetX - old.x, y: e.offsetY - old.y};
-		drawCircles();
-	}
+  if (move) {
+    var old = points[index];
+    points[index] = {x: e.offsetX, y: e.offsetY, v: {x: 0, y: 0}};
+    points[index].v = {x: e.offsetX - old.x, y: e.offsetY - old.y};
+    drawCircles();
+  }
 });
 
 setInterval(() => {
-	
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	drawCircles();
-	
-	
-}, 500 / 30);
 
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawCircles();
+
+
+}, 500 / 30);
 
